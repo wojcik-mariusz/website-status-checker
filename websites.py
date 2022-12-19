badf480bf39266d30ed95d8ffaa2d223b26d53a2
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-19 11:44:20 +0100

    update Website.__init__ docs

--- websites.py
+++ websites.py
@@ -4,6 +4,12 @@ from typing import Any
 
 class Website:
     def __init__(self, file_name: str):
+        """f
+        ile_name: file name with web names inside,
+        required to read name of websites
+        website_list: list contain dict of name of websites as key,
+        and some informations like status code as values
+        """
         self.file_name = file_name
         self.website_list: list[dict[str, Any]] = []
         self.index = 0
