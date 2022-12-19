dbef33ea19a8e1bc7de9fd21a5e78a6f499d3f3c
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-19 11:34:23 +0100

    init functions; load_file, get_next_website_to_check, put_website_data, save_report

--- websites.py
+++ websites.py
@@ -7,3 +7,15 @@ class Website:
         self.file_name = file_name
         self.website_list: list[dict[str, Any]] = []
         self.index = 0
+
+    def load_file(self):
+        pass
+
+    def get_next_website_to_check(self):
+        pass
+
+    def put_website_data(self):
+        pass
+
+    def save_report(self):
+        pass
