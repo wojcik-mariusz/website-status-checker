3866b037feed505a46354bfa5b4210e851d172e6
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-19 11:29:38 +0100

    Add class Website

---
+++ websites.py
@@ -0,0 +1,8 @@
+"""Services used to run main file"""
+from typing import Any
+
+
+class Website:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.website_list: list[dict[str, Any]] = []
